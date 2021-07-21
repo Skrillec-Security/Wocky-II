8f9f3fef187c8b1e72f3611eedbb7c4d95211e3e
n4n0 <benzothedev@gmail.com>
2021-07-21 08:52:39 -0400

    Update handler.v

--- server/handler.v
+++ server/handler.v
@@ -3,9 +3,7 @@ module server
 import net 
 
 pub fn cmd_handler(mut socket net.TcpConn, data string) {
-	for {
-		if data == "test" { 
-			socket.write_str("working") or { break }
-		}
+	if data == "test" { 
+		socket.write_str("working") or { break }
 	}
 }
