f56f2730c5ffa65a8c6d2f0da1c0bb66fdc1305e
n4n0 <benzothedev@gmail.com>
2021-07-22 08:13:02 -0400

    Update login.v

--- auth/login.v
+++ auth/login.v
@@ -1,12 +1,23 @@
 module auth
 
-pub struct Auth {
+pub struct AuthInfo {
 	pub mut:
 		username string
 		password string
 }
 
-pub fn (mut a Auth) login() ?string {
+pub fn (mut a AuthInfo) login() string {
 	mut test := Crud{user: a.username}
- 	return test.userline() + " | " + a.password
+ 	mut check_usr := test.userline()
+	mut usr_info := ['']
+	if check_usr.contains("[x]") { return check_usr } else { usr_info = check_usr.split(",") }
+	if usr_info[0] == (a.username).str() {
+		if usr_info[2] == (a.password).str() {
+			return "[+] Welcome: ${a.username}\r\n"
+		} else {
+			return "[x] Error, Username or password seems to be incorrect!\r\n"
+		}
+	} else {
+			return "[x] Error, Username or password seems to be incorrect!\r\n"
+	}
 }
