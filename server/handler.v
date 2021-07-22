f1189e563d249afeb9668f65af39501734ab4a26
n4n0 <benzothedev@gmail.com>
2021-07-22 08:17:35 -0400

    Update handler.v

--- server/handler.v
+++ server/handler.v
@@ -2,12 +2,25 @@ module server
 
 import net 
 
-pub struct ServerAssets {
-	clients []Client
+pub struct Clients {
+	id int
+mut:
+	conn net.TcpConn
 }
 
-pub fn cmd_handler(mut socket net.TcpConn, data string) {
-	if data == "test" { 
-		socket.write_str("working") or { break }
+pub struct ServerAssets {
+	clients []Clients
+}
+
+pub fn cmd_handler(mut socket net.TcpConn) {
+	socket.wait_for_read() or { panic("[x] Failed") }
+	data := socket.read_line().replace("\r\n", "")
+	if data.len != 0 {
+		if data == "test" { 
+			socket.write_str("working\r\n") or { panic("[x] Error") }
+			println("working")
+		}
+		socket.write_string("\r\x1b[37m╔═[\x1b[35mWocky\x1b[37m@\x1b[35mII\x1b[37m]\r\n╚════➢\x1b[32m ") or { panic("[x]") }
 	}
+	println(data)
 }
