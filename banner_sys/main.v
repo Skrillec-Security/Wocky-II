15e897d3214827e2331b127b5313b881455803a4
n4n0 <benzothedev@gmail.com>
2021-07-23 09:06:45 -0400

    Update main.v

--- banner_sys/main.v
+++ banner_sys/main.v
@@ -8,14 +8,20 @@ pub struct Banner {
 		current_banner string
 }
 
+pub fn (mut a Banner) set_bannerfile(filepath string) {
+	a.file = filepath
+}
+
 pub fn (mut a Banner) read_file() string {
 	mut c_dir := os.getwd()
-	mut fd = read_lines(c_dir + '/banner/${a.file}.txt') or {
+	mut fd := os.read_lines(c_dir + '/banners/${a.file}.txt') or {
 		panic("[x] Error, Couldn't read BANNER from file!\r\n")
 	}
+	mut new_str := ''
 	// convert []string to string with 'fd'
 	for line in fd {
 		a.current_banner += line + "\n"
+		new_str += line + "\n"
 	}
 
 	//Color the banner before returning
@@ -23,23 +29,23 @@ pub fn (mut a Banner) read_file() string {
 }
 
 pub fn (mut a Banner) color_banner() string {
-	mut g = ''
+	mut g := ''
 	g = (a.current_banner).replace("{RED}", "")
 	g = (a.current_banner).replace("{PURPLE}", "")
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
-	g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
+	// g = (a.current_banner).replace()
 	return g
 }
