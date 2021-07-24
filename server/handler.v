7982982358b055610e81bf674239c6ca1b82c1e9
n4n0 <benzothedev@gmail.com>
2021-07-24 00:34:23 -0400

    Update handler.v

--- server/handler.v
+++ server/handler.v
@@ -7,24 +7,15 @@ import config
 
 pub struct ServerAssets {
 	pub mut:
+		empty_c int
 		cnc_port string
 		clients []net.TcpConn
 }
 
-pub fn cmd_handler(mut socket net.TcpConn) {
+pub fn cmd_handler(mut socket net.TcpConn, data string) {
 	mut wocky := config.WockyInfo{creator: "vZy", version: "2.0.0"}
-	// socket.write_string()
-	socket.wait_for_read() or { panic("[x] Failed") }
-	data := socket.read_line().replace("\r\n", "")
-	mut empty_c := 0
 	if data.len == 0 {
-		empty_c += 1
-		if empty_c == 10 {
-			println('User disconnected ${socket.peer_addr()}')
-			socket.close() or {
-				panic("[x] Error")
-			}
-		}
+		socket.write_string("\r\x1b[37m╔═[\x1b[35mWocky\x1b[37m@\x1b[35mII\x1b[37m]\r\n╚════➢\x1b[32m ") or { panic("[x] Error, Failed to send hostname to socket!\r\n") }
 	} else {
 		mut c := config.Config{}
 		mut b := banner_sys.Banner{}
