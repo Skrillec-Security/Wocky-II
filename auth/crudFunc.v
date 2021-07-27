56b7baee1943153da552a366c208f98e26a54588
n4n0 <benzothedev@gmail.com>
2021-07-27 18:08:00 -0400

    Update crudFunc.v

--- auth/crudFunc.v
+++ auth/crudFunc.v
@@ -18,24 +18,39 @@ struct CrudFunc {
 //         the max rank might be live updatable
 pub fn (mut cf CrudFunc) is_premium() int {
 	mut s := Crud{user: cf.user}
-	mut usr_info := s.read_line()
+	mut usr_info := s.userline()
 	if usr_info.contains("[x]") {
-		return usr_info
-	}
-
-	usr_info = usr_info.split(",")
-
-	if usr_info[3].int() > 0 or usr_info[3].int() <= 6 {
-		return usr_info
-	} else {
 		return 0
 	}
 
+	if s.lvl > 0 {
+		return s.lvl
+	} else if s.lvl < 6 {
+		return s.lvl
+	} else if s.lvl == 6 {
+		return s.lvl
+	} else {
+		return 0
+	}
 }
 
 // method -> is_admin() int 
 // note -> check if the user is admin!
 //         upto 3 ranks (0 = no admin | 1 = reseller | 2 = admin | 3 = owner)
 pub fn (mut cf CrudFunc) is_admin() int {
+	mut s := Crud{user: cf.user}
+	mut usr_info := s.userline()
+	if usr_info.contains("[x]") {
+		return 0
+	}
 
+	if s.admin > 0 {
+		return s.admin
+	} else if s.admin <= 3 {
+		return s.admin
+	} else {
+		return 0
+	}
 }
+
+// pub fn (mut cf CrudFunc) is_
