85c28194d0ccaa686826d7b8f29b36655a888624
n4n0 <benzothedev@gmail.com>
2021-07-27 18:09:04 -0400

    Update CLI.v

--- utils/CLI.v
+++ utils/CLI.v
@@ -7,3 +7,7 @@ pub struct CLI{}
 pub fn (mut c CLI) resize_terminal(mut socket net.TcpConn, r int, col int) {
 	socket.write_string("\033[8;${r};${col}t") or { panic("[x] Error, Unable to write to socket")}
 }
+
+pub fn (mut c CLI) set_title(mut socket net.TcpConn, msg string) {
+	socket.write_string("\033]0;${msg}\007") or { panic("[x] Error, Unable to write to socket") }
+}
