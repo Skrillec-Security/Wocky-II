152ed0d1293ae3b7c58b97f6aa9b223b674264ff
n4n0 <benzothedev@gmail.com>
2021-07-27 18:07:49 -0400

    Update attackFunc.v

--- auth/attackFunc.v
+++ auth/attackFunc.v
@@ -11,24 +11,25 @@ pub struct AttackFunc {
 
 pub fn (mut af AttackFunc) attack_validation() bool {
 	mut c := Crud{user: af.user}
-	mut info := c.userline()
-	if info.contains("[x]") { return info }
-	info = info.split(",")
-	if info[5].int() == info[6].int() {
+	mut info_check := c.userline()
+	if info_check.contains("[x]") { return false }
+	if c.conn == c.ongoing {
 		return false
 	} else {
 		return true
 	}
 }
 
-pub fn (mut af AttackFunc) validation_time() bool {
+pub fn (mut af AttackFunc) validate_time() bool {
 	mut c := Crud{user: af.user}
-	mut info := c.userline()
-	if info.contains("[x]") { return info }
-	info = info.split(",")
-	if info[4].int() =< af.time {
-		return false
-	} else {
+	mut info_check := c.userline()
+	if info_check.contains("[x]") { return false }
+	mut info := info_check.split(",")
+	if c.mtime > af.time{
 		return true
+	} else if c.mtime == af.time {
+		return true
+	} else {
+		return false
 	}
 }
