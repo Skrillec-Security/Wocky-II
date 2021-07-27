6483264b7ca09da55f6341d3ec5ffe6c0829cde5
n4n0 <benzothedev@gmail.com>
2021-07-26 20:27:27 -0400

    Update attack.v

--- commands/attack.v
+++ commands/attack.v
@@ -1,14 +1,11 @@
-module attackcmd
+module commands
 
-pub struct AttackInfo {
-	pub mut:
-		ip string
-		port int
-		time int
-		method string
+import net
+import config
+import api_attack
+
+pub fn attack_cmd(mut socket net.TcpConn, arg []string, usern string) {
+	println('test 2')
+	mut api_attk := api_attack.API_Attack_Info{}
+	socket.write_string((api_attk.send_attack(usern, arg[1], arg[2].int(), arg[3].int(), arg[4]))) or { panic("[x] Error, Unable to send attack response!\r\n")}
 }
-
-pub fn (mut info AttackInfo) attack() string {
-	
-}
-
