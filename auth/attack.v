c63ef68488f93f6045ccf970ccce9002693b8d12
n4n0 <benzothedev@gmail.com>
2021-07-26 20:23:06 -0400

    Update attack.v

--- auth/attack.v
+++ auth/attack.v
@@ -1,5 +1,8 @@
 module auth
 
+import auth
+import os
+
 pub struct AttackCrud {
 	pub mut:
 		user string
@@ -8,27 +11,26 @@ pub struct AttackCrud {
 // method -> user_conn_up() 
 // note -> 
 pub fn (mut ac AttackCrud) user_conn_up() {
-	mut c := Crud{ac.user}
-	mut info := c.userline()
-	if info.contains("[x]") { return info }
-	info = info.split(",")
+	mut c := auth.Crud{user: ac.user}
+	mut info_check := c.userline()
+	if info_check.contains("[x]") { return }
+	mut info := info_check.split(",")
 	mut new_conn := info[6].int()+1
 	c.user_remove()
 	mut fd := os.open("/root/Wocky/db/users.db") or { panic("[x] Error, Unable to read USER database!\r\n") }
-	fd.write("('${info[0]}','${info[1]}','${info[2]}','${info[3]}','${info[4]}','${info[5]}','${new_conn}','${info[7]}','${info[8]}')\n".bytes())
+	fd.write("('${info[0]}','${info[1]}','${info[2]}','${info[3]}','${info[4]}','${info[5]}','${new_conn}','${info[7]}','${info[8]}')\n".bytes()) or { panic("[x] Error, Unable to read USER database!\r\n") }
 	fd.close()
 }
-
 // method -> user_conn_down() 
 // note -> 
 pub fn (mut ac AttackCrud) user_conn_down() {
-	mut c := Crud{ac.user}
-	mut info := c.userline()
-	if info.contains("[x]") { return info }
-	info = info.split(",")
+	mut c := auth.Crud{user: ac.user}
+	mut info_check := c.userline()
+	if info_check.contains("[x]") { return }
+	mut info := info_check.split(",")
 	mut new_conn := info[6].int()-1
 	c.user_remove()
 	mut fd := os.open("/root/Wocky/db/users.db") or { panic("[x] Error, Unable to read USER database!\r\n") }
-	fd.write("('${info[0]}','${info[1]}','${info[2]}','${info[3]}','${info[4]}','${info[5]}','${new_conn}','${info[7]}','${info[8]}')\n".bytes())
+	fd.write("('${info[0]}','${info[1]}','${info[2]}','${info[3]}','${info[4]}','${info[5]}','${new_conn}','${info[7]}','${info[8]}')\n".bytes()) or { panic("[x] Error, Unable to read USER database!\r\n")}
 	fd.close()
 }
