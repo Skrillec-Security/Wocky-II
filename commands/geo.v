88d37c322a96c9bbf2bf8e3f787aab37d1988308
n4n0 <benzothedev@gmail.com>
2021-07-30 07:18:12 -0400

    Update geo.v

--- commands/geo.v
+++ commands/geo.v
@@ -1,6 +1,9 @@
+module commands
+
+import net
 import net.http
 
-fn geo_cmd(mut socket net.TcpConn, ip string) {
+pub fn geo_cmd(mut socket net.TcpConn, ip string) {
 	mut geo := http.get_text("http://ip-api.com/json/{query}${ip}")
 	geo = ((geo.replace(",", "\r\n")).replace('"', "")).replace(":", "   :   ")
 	socket.write_string(geo) or { panic("[x] Error, Unable to send geo response!") }
