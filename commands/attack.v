51aa617437819c5d8453bc90ff8ee6939da2f165
n4n0 <benzothedev@gmail.com>
2021-08-04 19:48:44 -0400

    Update attack.v

--- commands/attack.v
+++ commands/attack.v
@@ -5,6 +5,6 @@ import config
 import api_attack
 
 pub fn attack_cmd(mut socket net.TcpConn, arg []string, usern string) {
-	mut api_attk := api_attack.API_Attack_Info{user: usern}
+	mut api_attk := api_attack.API_Attack_Info{}
 	socket.write_string((api_attk.send_attack(usern, arg[1], arg[2].int(), arg[3].int(), arg[4]))) or { panic("[x] Error, Unable to send attack response!\r\n")}
 }
