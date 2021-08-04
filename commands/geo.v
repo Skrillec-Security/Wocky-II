08440fa899627cefbef564845c3cb0ce557cef4f
n4n0 <benzothedev@gmail.com>
2021-08-04 19:50:31 -0400

    Update geo.v

--- commands/geo.v
+++ commands/geo.v
@@ -3,8 +3,13 @@ module commands
 import net
 import net.http
 
-pub fn geo_cmd(mut socket net.TcpConn, ip string) {
-	mut geo := http.get_text("http://ip-api.com/json/{query}${ip}")
-	geo = ((geo.replace(",", "\r\n")).replace('"', "")).replace(":", "   :   ")
-	socket.write_string(geo) or { panic("[x] Error, Unable to send geo response!") }
+pub fn geo_cmd(mut socket net.TcpConn, data string) {
+	mut arg := data.split(" ")
+	if arg.len == 1 {
+		mut geo := http.get_text("http://ip-api.com/json/{query}${ip}")
+		geo = ((geo.replace(",", "\r\n")).replace('"', "")).replace(":", "   :   ")
+		socket.write_string(geo) or { 0 }
+	} else {
+		socket.write_string("[x] Error, Invalid argument\r\nUsage: geo <ip>\r\n") or { 0 }
+	}
 }
