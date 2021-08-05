7226595ef8907aa48873bae6a53517199d015e1d
n4n0 <benzothedev@gmail.com>
2021-08-05 18:47:12 -0400

    Create user_table.js

---
+++ js_backend/user_table.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+function t_test(Username, IP, Password, Level, Maxtime, Conn, OnGoing, Admin, Expiry) {
+    this.Username = Username
+    this.IP = IP
+    this.Password = Password
+    this.Level = Level
+    this.Maxtime = Maxtime
+    this.Conn = Conn
+    this.OnGoing = OnGoing
+    this.Admin = Admin
+    this.Expiry = Expiry
+}
+// will change this to read from FILE!. this is just gonnah stay like this for my net!
+var skid = fs.readFileSync('/root/Wocky/db/users.db', "utf8").split("\n"); 
+var fag = []
+skid.forEach(e => {
+    if(e.length > 5) {
+        let lol = ((e.split("('").join("")).split("')").join("")).split("','")
+        fag.push(new t_test(lol[0], lol[1], lol[2], lol[3], lol[4], lol[5], lol[6], lol[7], lol[8].split("\r").join("")))
+    }
+})
+console.table(fag)
