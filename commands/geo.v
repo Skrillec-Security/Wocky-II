1779037ccb687655e98f990a1a1387ba24b45055
n4n0 <benzothedev@gmail.com>
2021-08-05 16:19:45 -0400

    Update geo.v

--- commands/geo.v
+++ commands/geo.v
@@ -6,7 +6,7 @@ import net.http
 pub fn geo_cmd(mut socket net.TcpConn, data string) {
 	mut arg := data.split(" ")
 	if arg.len == 1 {
-		mut geo := http.get_text("http://ip-api.com/json/{query}${ip}")
+		mut geo := http.get_text("http://ip-api.com/json/${arg[1]}")
 		geo = ((geo.replace(",", "\r\n")).replace('"', "")).replace(":", "   :   ")
 		socket.write_string(geo) or { 0 }
 	} else {
