a6f95e42328d6f97255d02d501d1289c70573fd9
n4n0 <benzothedev@gmail.com>
2021-08-05 20:40:52 -0400

    Update js_to_vlang.v

--- js_backend/js_to_vlang.v
+++ js_backend/js_to_vlang.v
@@ -2,9 +2,9 @@ module js_backend
 
 import os
 
-pub struct JS {}
+pub struct JsFunc{}
 
-pub fn (mut js JS) user_table() string {
+pub fn (mut js JsFunc) user_table() string {
 	mut response := os.execute("node user_table.js").output
 	response = response.replace("─", "═")
 	response = response.replace("│", "║")
