faa46a2bb7349a1cf76bc42daa627f71063c5e1d
n4n0 <benzothedev@gmail.com>
2021-08-07 17:45:54 -0400

    Update user_table.js

--- js_backend/user_table.js
+++ js_backend/user_table.js
@@ -1,9 +1,8 @@
 var fs = require('fs');
 
-function t_test(Username, IP, Password, Level, Maxtime, Conn, OnGoing, Admin, Expiry) {
+function t_test(Username, IP, Level, Maxtime, Conn, OnGoing, Admin, Expiry) {
     this.Username = Username
     this.IP = IP
-    this.Password = Password
     this.Level = Level
     this.Maxtime = Maxtime
     this.Conn = Conn
@@ -17,7 +16,7 @@ var fag = []
 skid.forEach(e => {
     if(e.length > 5) {
         let lol = ((e.split("('").join("")).split("')").join("")).split("','")
-        fag.push(new t_test(lol[0], lol[1], lol[2], lol[3], lol[4], lol[5], lol[6], lol[7], lol[8].split("\r").join("")))
+        fag.push(new t_test(lol[0], lol[1], lol[3], lol[4], lol[5], lol[6], lol[7], lol[8].split("\r").join("")))
     }
 })
 console.table(fag)
