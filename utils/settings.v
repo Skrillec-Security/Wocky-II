f6f1d7894b9f2605e5f96c77c1f246031c9c5772
n4n0 <benzothedev@gmail.com>
2021-08-12 15:14:23 -0400

    Create settings.v

---
+++ utils/settings.v
@@ -0,0 +1,13 @@
+module utils
+
+import os
+pub struct Wocky_Settings{}
+
+pub fn (mut w_s Wocky_Settings) get_settings() []string {
+	mut settingz := os.read_lines(os.getwd() + "/config/settings.cfg") or { panic("[x] Error, Unable to read settings\r\nYou need the file with the correct setting info in it to run this net!") }
+	mut info := []string
+	info << settingz[0].replace("terminal_size=", "")
+	info << settingz[1].replace("terminal_title=", "")
+	info << settingz[2].replace("Wocky_MOTD=", "")
+	return info
+}
