59736ed0b91ceb130addb9b56dc2e81b72750bd4
n4n0 <benzothedev@gmail.com>
2021-08-12 15:12:31 -0400

    Delete build.v

--- build.v
+++
@@ -1,9 +0,0 @@
-
-
-fn main() {
-	os.execute("yum update -y -q").ouput
-	os.execute("yum install python3").ouput
-	os.execute("yum install python3-pip -y -q").ouput
-	os.execute("pip3 install requests").ouput
-	os.execute("pip3 install discord_webhook").ouput
-}
