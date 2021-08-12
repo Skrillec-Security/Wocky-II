e7b0bf1217e163e732aee1464baa3f51871a18ce
n4n0 <benzothedev@gmail.com>
2021-08-12 15:14:39 -0400

    Update wocky.v

--- wocky.v
+++ wocky.v
@@ -8,6 +8,8 @@
 import server
 import auth
 import wocky_cp
+import wocky_uix
+import wocky_utils
 import config
 import banner_sys
 import utils
@@ -21,6 +23,8 @@ import time
 // fn read(int, string, int) int
 fn C.check()
 // fn C.fdopen(int, string)
+
+struct Current {}
 
 fn main() {
 	C.check()
@@ -51,23 +55,25 @@ fn listener(port string) {
 
 fn handle_client(mut socket net.TcpConn) {
 	mut u := utils.CLI{}
-	u.set_title(mut socket, "Wocky II | Login")
-	u.resize_terminal(mut socket, 40, 85)
+	mut c_s := utils.Wocky_Settings{}
+	mut wuix := wocky_uix.UIX_Func{}
+	mut b := banner_sys.Banner{file: "ui"}
+	u.set_title(mut socket, "${c_s.get_settings()[1]} | Login") // wow just wow. im to high for this LOL
+	u.resize_terminal(mut socket, 20, 87)
 	mut reader := io.new_buffered_reader(reader: socket)
 	mut current_ip := socket.peer_addr() or { return } //User's IP
 	println('> new client: $current_ip')
 	socket.set_read_timeout(time.infinite)
 
-	// Login Sections 
+	/*
+	Logging In System!
+	*/ 
 	socket.write_string("Username: ") or { 0 }
-	// socket.wait_for_read() or { panic("[x] Error") }
-	mut username := reader.read_line()
+	mut username := reader.read_line() or { "" }
 	username = username.replace("\r\n", "")
 	socket.write_string("Password: ") or { 0 }
-	// socket.wait_for_read() or { panic("[x] Error") }
-	mut password := reader.read_line()
+	mut password := reader.read_line() or { "" }
 	password = password.replace("\r\n", "")
-
 	mut a := auth.AuthInfo{username: username, password: password}
 	mut login := a.login()
 	if login.contains("[+]") {
@@ -78,18 +84,22 @@ fn handle_client(mut socket net.TcpConn) {
 		socket.write_string(login) or { 0 }
 		socket.close() or { panic("[x] Error, Failed to close the socket!") }
 	}
-	mut b := banner_sys.Banner{file: "main"}
-	socket.write_string(config.Clear + b.color_banner()) or { 0 }
-	b.set_bannerfile("dashboard")
-	socket.write_string(b.color_banner()) or { 0 }
+
+	/*
+	Command Handler
+	*/
+
 	
 	mut empty_c := 0
-	// Login Successfully Below
-	u.set_title(mut socket, "Wocky II | User: $username")
-	socket.write_string(config.Hostname) or { 0 }
-
+	u.set_title(mut socket, "${c_s.get_settings()[1]} | User: $username")
+	socket.write_string(config.Clear) or { 0 }
+	b.start_output(mut socket)
+	b.set_bannerfile("text")
+	b.read_banner_text(mut socket)
+	wuix.sock_move_cursor(mut socket, 19, 37)
 	for {
-		mut data := reader.read_line()
+		// u.set_title(mut socket, "${c_s.get_settings()[1]} | User: $username")
+		mut data := reader.read_line() or { "" }
 		data = data.replace("\r\n", "")
 		if data.len == 0 {
 			empty_c += 1
