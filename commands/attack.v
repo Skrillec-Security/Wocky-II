aa754682000faa38f4a4b3bd39c80a19cfaab5f1
n4n0 <benzothedev@gmail.com>
2021-08-18 15:02:54 -0400

    Update attack.v

--- commands/attack.v
+++ commands/attack.v
@@ -3,8 +3,16 @@ module commands
 import net
 import config
 import api_attack
+import wocky_uix
 
 pub fn attack_cmd(mut socket net.TcpConn, arg []string, usern string) {
 	mut api_attk := api_attack.API_Attack_Info{}
-	socket.write_string((api_attk.send_attack(usern, arg[1], arg[2].int(), arg[3].int(), arg[4]))) or { panic("[x] Error, Unable to send attack response!\r\n")}
+	mut wuix := wocky_uix.UIX_Func{}
+	mut response := api_attk.send_attack(usern, arg[1], arg[2].int(), arg[3].int(), arg[4])
+	if response.len > 57 {
+		for i in 0..57 {
+			println(response[i])
+		}
+	}
+	wuix.sock_place_text(mut socket, 5, 29, "►")
 }
