44ba24a84602f5583c2032ec34076a711b508287
n4n0 <benzothedev@gmail.com>
2021-08-18 15:02:40 -0400

    Delete test.c

--- c_headers/test.c
+++
@@ -1,9 +0,0 @@
-int check(void)
-{
-  #ifdef __unix__
-    printf("[+] Linux System!\r\n");    
-  #elif defined(_WIN32) || defined(WIN32)
-    printf("This system is not on linux!\r\n");
-    exit(0);
-  #endif
-}
