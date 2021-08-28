6372d21d615dfe7a70917334d2591892769f0c0e
n4n0 <benzothedev@gmail.com>
2021-08-28 12:20:35 -0400

    Update main.v

--- api_attack/main.v
+++ api_attack/main.v
@@ -4,18 +4,25 @@ import api_attack
 import auth
 import net.http
 import os
+import wocky_utils
 
-pub struct API_Attack_Info {}
+pub struct API_Attack_Info {
+	pub mut:
+		apis_found int
+		response string
+}
 
 pub fn (mut s API_Attack_Info) send_attack(usr string, ip string, port int, time int, method string) string {
 	mut api_c := api_attack.API_Crud{}
 	mut cf := auth.CrudFunc{user: usr}
 	mut attk_c := auth.AttackCrud{user: usr}
 	mut attk_f := auth.AttackFunc{user: usr}
+	mut wutils := wocky_utils.Custom_utils{}
 	if cf.is_premium() == true {
 		if attk_f.attack_validation() {
 			if attk_f.validate_time() {
 				mut apis := api_c.apis_with_method(method) // this function return an array of APIs
+				s.apis_found = apis.len
 				// println(apis)
 				mut resp := ''
 				for api in apis { 
@@ -33,6 +40,10 @@ pub fn (mut s API_Attack_Info) send_attack(usr string, ip string, port int, time
 					}
 					// println(f) // print response to make sure attack is going thru
 				}
+				s.response = resp
+				// if wutils.str_contain_count(s.response, "\n") == s.apis_found {
+				// 	// send with 's.apis_found' apis
+				// }
 				// attk_c.user_conn_up() // up the user on-going attk
 				return resp
 			} else {
