679cf5e3cf454b38b8a2f2658f2115f633c073c3
n4n0 <benzothedev@gmail.com>
2021-08-28 12:20:48 -0400

    Update attack.v

--- auth/attack.v
+++ auth/attack.v
@@ -17,7 +17,7 @@ pub fn (mut ac AttackCrud) user_conn_up() {
 	mut info := info_check.split(",")
 	mut new_conn := info[6].int()+1
 	c.user_remove()
-	mut fd := os.open("/root/Wocky/db/users.db") or { panic("[x] Error, Unable to read USER database!\r\n") }
+	mut fd := os.open(os.getwd() + "/db/users.db") or { panic("[x] Error, Unable to read USER database!\r\n") }
 	fd.write("('${info[0]}','${info[1]}','${info[2]}','${info[3]}','${info[4]}','${info[5]}','${new_conn}','${info[7]}','${info[8]}')\n".bytes()) or { panic("[x] Error, Unable to read USER database!\r\n") }
 	fd.close()
 }
@@ -30,7 +30,7 @@ pub fn (mut ac AttackCrud) user_conn_down() {
 	mut info := info_check.split(",")
 	mut new_conn := info[6].int()-1
 	c.user_remove()
-	mut fd := os.open("/root/Wocky/db/users.db") or { panic("[x] Error, Unable to read USER database!\r\n") }
+	mut fd := os.open(os.getwd() + "/db/users.db") or { panic("[x] Error, Unable to read USER database!\r\n") }
 	fd.write("('${info[0]}','${info[1]}','${info[2]}','${info[3]}','${info[4]}','${info[5]}','${new_conn}','${info[7]}','${info[8]}')\n".bytes()) or { panic("[x] Error, Unable to read USER database!\r\n")}
 	fd.close()
 }
