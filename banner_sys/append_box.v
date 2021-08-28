58a9fa055e5e567956daa319bc502515f6d7a73d
n4n0 <benzothedev@gmail.com>
2021-08-28 14:01:17 -0400

    Create append_box.v

---
+++ banner_sys/append_box.v
@@ -0,0 +1,20 @@
+module banner_sys
+
+// Mode for data type usage
+// 1 = line
+// 2 = lines
+pub struct Box{
+	pub mut:
+		box string
+		lines []string
+		line string
+		mode int
+}
+
+pub fn (mut b Box) set_mode(m int) {
+	b.mode = m
+}
+
+pub fn (mut b Box) append_arr(lines []string) {
+
+}
