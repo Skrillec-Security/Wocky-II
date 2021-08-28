ef6140a821ce9b63c5699f3dbcf9a27b29cfae69
n4n0 <benzothedev@gmail.com>
2021-08-28 17:32:08 -0400

    Update handler.v

--- server/handler.v
+++ server/handler.v
@@ -85,6 +85,10 @@ pub fn cmd_handler(mut socket net.TcpConn, data string, username string) {
 				b.set_bannerfile("toxic_stress")
 				b.read_banner_text(mut socket)
 			}
+			"astra" {
+				b.set_bannerfile("astra")
+				b.read_banner_text(mut socket)
+			}
 			"geo" {
 				commands.geo_cmd(mut socket, data)
 			} 
